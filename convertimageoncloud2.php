da4a3289e874f21e758a14f14137bbab5faf1c55
Shakeel Faiz <shakeel.faiz@aspose.com>
2021-10-25 16:02:59 +0500

    Update convertimageoncloud2.php

--- convertimageoncloud2.php
+++ convertimageoncloud2.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ ."/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use \Aspose\Imaging\ImagingApi;
 use \Aspose\Imaging\Configuration;
@@ -11,17 +11,26 @@ use \Aspose\Imaging\Model\Requests\CreateConvertedImageRequest;
 
 $config = new Configuration();
 $config->setBaseUrl($config->getBaseUrl());
-$config->setClientId("xxxxxx");
-$config->setClientSecret("xxxxxxx");
+$config->setClientId("81e42949-3dc6-4080-b1d7-1c2c30cd954c");
+$config->setClientSecret("5cfcf33020020c094b481ea11e25da04");
 
 $api = new ImagingApi($config);
 
-$imageData = file_get_contents("sample.png");
+$filename = "sample.jpg";
 
-$req = new CreateConvertedImageRequest($imageData,"png");
+$filenamewithoutext = strtolower(pathinfo($filename, PATHINFO_FILENAME));
+$ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+$format = $ext;
+$outputfilename = $filenamewithoutext . ".out." . $ext;
+
+$sizeBefore = filesize($filename);
+$imageData = file_get_contents($filename);
+
+$req = new CreateConvertedImageRequest($imageData, $format);
 $res = $api->createConvertedImage($req);
 
+$sizeAfter = $res->getSize();
 $cont = $res->getContents();
-file_put_contents("sample.out.png", $cont);
+file_put_contents($outputfilename, $cont);
 
-$i=1;
+$i = 1;
